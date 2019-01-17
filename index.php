bae2b150b2a58327a026756a8bfd55ed8f922f1e
admir <admir.osmanovic@hhinc.eu>
2019-01-17 12:01:52 +0100

    update metadata

--- index.php
+++ index.php
@@ -9,7 +9,20 @@
 echo "<b>Ovo je testna php stranica. </b> \n <br>";
 echo "<b>Admir Osmanovic </b> \n <br>";
 echo "<b>Ovo je testna web stranica </b> \n <br>";
-echo "HH Inc."
+echo "HH Inc.";
+<div>
+echo "<b>Instance ID: </b> ";
+$instance_id = file_get_contents("http://169.254.169.254/latest/meta-data/instance-id");
+echo $instance_id, "\n <br>";
+echo "<b>Instance Region: </b>";
+$instance_region = file_get_contents("http://169.254.169.254/latest/meta-data/placement/availability-zone");
+echo $instance_region, "\n <br>";
+
+echo "<b>Instance Private-IP: </b>";
+$instance_private_ip = file_get_contents("http://169.254.169.254/latest/meta-data/hostname");
+echo $instance_private_ip, "\n <br>";
+</div>
+
 ?> 
 
 </body>
