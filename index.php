ffecffc7707554b6d8842708c0ed773158edaec7
admir93 <45433544+admir93@users.noreply.github.com>
2019-01-25 11:36:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <body>
 
 <h1>PHP Jenkins test page</h1>
-<h1>Update</h1>
-<h4>Share bez d</h4>
+
 <?php
 echo "<b>Ovo je testna php stranica. </b> \n <br>";
 echo "<b>Admir Osmanovic </b> \n <br>";
@@ -24,6 +23,12 @@ echo $instance_private_ip, "\n <br>";
 
 ?> 
 
+  <div align=center>
+    <h2>Watchdog</h2>
+    
+    <img src="doge.jpg" >
+   </div>
+  
 </body>
 </html>
 
